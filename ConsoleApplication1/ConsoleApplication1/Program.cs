3d8c65d15be8a669e70ee2c1131e95e68255b561
Josh Levine <joshua.s.levine@asu.edu>
2015-10-22 11:11:40 -0700

    Change console output message

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello there!");
+            Console.WriteLine("Different console message");
         }
     }
 }
